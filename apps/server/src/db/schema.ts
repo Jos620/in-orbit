4f6de7f9e8cc9d773f1ac1ad69c5681606ce44a1
Mateus Ito <mateusito10@gmail.com>
2024-09-10 15:21:23 -0300

    feat(server/db): create goals completions table

--- apps/server/src/db/schema.ts
+++ apps/server/src/db/schema.ts
@@ -9,3 +9,13 @@ export const goals = pgTable('goals', {
     .notNull()
     .defaultNow(),
 })
+
+export const goalCompletions = pgTable('goals_completions', {
+  id: text('id').primaryKey().$defaultFn(createId),
+  goalId: text('goal_id')
+    .references(() => goals.id)
+    .notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
