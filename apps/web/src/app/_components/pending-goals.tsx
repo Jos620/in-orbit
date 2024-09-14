e4213732c43045029a1b64393111bca3eb47cc5d
Mateus Ito <mateusito10@gmail.com>
2024-09-14 14:17:54 -0300

    feat(web): complete goals

--- apps/web/src/app/_components/pending-goals.tsx
+++ apps/web/src/app/_components/pending-goals.tsx
@@ -1,12 +1,25 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { Plus } from 'lucide-react'
 import { OutlineButton } from '~/components/ui/outline-button'
+import { completeGoal } from '~/services/complete-goal'
 import { getPendingGoals } from '~/services/get-pending-goals'
 
 export function PendingGoals() {
+  const queryClient = useQueryClient()
+
   const { data: pendingGoals } = useQuery({
     queryKey: ['pending-goals'],
     queryFn: getPendingGoals,
+  })
+
+  const completeGoalMutation = useMutation({
+    mutationKey: ['complete-goal'],
+    mutationFn: async (goalId: string) => {
+      await completeGoal(goalId)
+
+      queryClient.invalidateQueries({ queryKey: ['summary'] })
+      queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+    },
   })
 
   return (
@@ -15,6 +28,7 @@ export function PendingGoals() {
         <OutlineButton
           key={goal.id}
           disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
+          onClick={() => completeGoalMutation.mutate(goal.id)}
         >
           <Plus className="size-4 text-zinc-600" />
           {goal.title}
